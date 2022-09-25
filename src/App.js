7b4269b836b57f8fd7b976a9982c2ece8f3691b0
NikDoe <nikd0e@yandex.ru>
2022-09-25 11:59:05 +0300

    feature(schedule): fetching data from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello there 😉</h1>
-    </div>
-  );
+	const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+	const [data, setData] = useState([]);
+	const [error, setError] = useState(null);
+	const [loading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(API_URL);
+				if (!response.ok) throw Error('ошибка при получении данных👽');
+				const data = await response.json();
+				setData(data);
+				setError(null);
+			} catch (error) {
+				setError(error.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<div className="App">
+			{loading && <h1>Загрузка...</h1>}
+			{error && <h1>{error}</h1>}
+			{!error &&
+				!loading &&
+				data.map((obj, index) => (
+					<h1 key={index}>
+						{index + 1} {obj.title}
+					</h1>
+				))}
+		</div>
+	);
 }
 
 export default App;
