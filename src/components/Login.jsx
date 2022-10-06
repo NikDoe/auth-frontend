5b2928fab07c9365f4ea63c0b9d0ecd3c136d6ee
NikDoe <nikd0e@yandex.ru>
2022-10-06 12:01:11 +0300

    feature(schedule): add login form submition

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,59 +1,86 @@
 import { useRef, useState, useEffect, useContext } from 'react';
-import AuthContext from '../context/AuthProvider'
+import AuthContext from '../context/AuthProvider';
+import axios from '../api/axios';
 
 const Login = () => {
-    const { setAuth } = useContext(AuthContext);
-    const userRef = useRef();
-    const errRef = useRef();
+	const { setAuth } = useContext(AuthContext);
+	const userRef = useRef();
+	const errRef = useRef();
 
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+	const [user, setUser] = useState('');
+	const [pwd, setPwd] = useState('');
+	const [errMsg, setErrMsg] = useState('');
+	const [success, setSuccess] = useState(false);
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
+	useEffect(() => {
+		userRef.current.focus();
+	}, []);
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
+	useEffect(() => {
+		setErrMsg('');
+	}, [user, pwd]);
 
-    return (
-        <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h1>Вход в аккаунт</h1>
-                    <form>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                        />
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-                        <label htmlFor="password">Пароль:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                        />
-                        <button>Войти</button>
-                    </form>
-                    <p>
-                        Нужен аккаунт?<br />
-                        <span className="line">
-                            {/*put router link here*/}
-                            <a href="#">Зарегистрироваться</a>
-                        </span>
-                    </p>
-                </section>
-    )
-}
+		try {
+			const response = await axios.post('/auth', JSON.stringify({ user, password: pwd }), {
+				headers: { 'Content-Type': 'application/json' },
+				withCredentials: true,
+			});
+			console.log(JSON.stringify(response?.data));
+			
+		} catch (err) {
+			if (!err?.response?.data) {
+				setErrMsg('Нет ответа от сервера');
+			} else if (err.response?.status === 400) {
+				setErrMsg(err.response.data.message);
+			} else if (err.response?.status === 401) {
+				setErrMsg(err.response.data.message);
+			} else {
+				setErrMsg('Не удалось войти');
+			}
+		}
+	}
+
+	return (
+		<section>
+			<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
+				{errMsg}
+			</p>
+			<h1>Вход в аккаунт</h1>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="username">Username:</label>
+				<input
+					type="text"
+					id="username"
+					ref={userRef}
+					autoComplete="off"
+					onChange={e => setUser(e.target.value)}
+					value={user}
+					required
+				/>
+
+				<label htmlFor="password">Пароль:</label>
+				<input
+					type="password"
+					id="password"
+					onChange={e => setPwd(e.target.value)}
+					value={pwd}
+					required
+				/>
+				<button>Войти</button>
+			</form>
+			<p>
+				Нужен аккаунт?
+				<br />
+				<span className="line">
+					{/*put router link here*/}
+					<a href="#">Зарегистрироваться</a>
+				</span>
+			</p>
+		</section>
+	);
+};
 
 export default Login;
