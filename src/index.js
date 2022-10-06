5427ded72fc9828bd91ea90c273f70003d2dfe70
NikDoe <nikd0e@yandex.ru>
2022-10-06 10:13:16 +0300

    feature(schedule): add context as global state for auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from './context/AuthProvider';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+	<AuthProvider>
+		<App />
+	</AuthProvider>,
+);
