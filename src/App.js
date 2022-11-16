2096c667139350925ade91806fd3cfe7556ecc98
NikDoe <nikd0e@yandex.ru>
2022-11-16 13:06:51 +0300

    feature(schedule): add react router dom to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import LinkPage from './components/LinkPage';
 import Login from './components/Login';
 import Register from './components/Register';
+import Unauthorized from './components/Unauthorized';
+import Editor from './components/Editor';
+import Admin from './components/Admin';
+import Lounge from './components/Lounge';
+import Missing from './components/Missing';
 
 function App() {
 	return (
-		<main className="App">
-			{/* <Register /> */}
-			<Login />
-		</main>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				{/* public routes */}
+				<Route path="login" element={<Login />} />
+				<Route path="register" element={<Register />} />
+				<Route path="linkpage" element={<LinkPage />} />
+				<Route path="unauthorized" element={<Unauthorized />} />
+
+				{/* we want to protect these routes */}
+				<Route path="/" element={<Home />} />
+				<Route path="editor" element={<Editor />} />
+				<Route path="admin" element={<Admin />} />
+				<Route path="lounge" element={<Lounge />} />
+
+				{/* catch all */}
+
+				<Route path="*" element={<Missing />} />
+			</Route>
+		</Routes>
 	);
 }
 
