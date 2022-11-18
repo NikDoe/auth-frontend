190968254175bfb1087d091040650915cb28a997
NikDoe <nikd0e@yandex.ru>
2022-11-18 13:48:12 +0300

    feature(schedule): get users list for admin
    - create users component;
    - refresh accessToken w/ interceptors;

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
+const BASE_URL = 'https://shedule-backend.onrender.com/api';
 
 export default axios.create({
-	baseURL: 'https://shedule-backend.onrender.com/api',
+	baseURL: BASE_URL,
+});
+
+export const axiosPrivate = axios.create({
+	baseURL: BASE_URL,
 });
--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import Users from '../components/Users';
 
 const Admin = () => {
 	return (
 		<section>
 			<h1>Админка</h1>
 			<br />
-			<p>только Администратор может посещать эту страницу.</p>
+			<Users />
 			<div className="flexGrow">
 				<Link to="/">на главную</Link>
 			</div>
