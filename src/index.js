3d6c76f523a31321fdfd32c93b14513ca981d4a6
NikDoe <nikd0e@yandex.ru>
2022-11-20 12:28:03 +0300

    feature(auth): disable react devtools in prod mode

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import './index.scss';
+
+if (process.env.NODE_ENV === 'production') {
+	disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
